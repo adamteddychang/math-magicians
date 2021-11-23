6de39f175e56cf526e81442f7b85f5462d599869
adamteddychang <adamchang0725@gmail.com>
2021-11-23 19:30:29 +0100

    fix

--- src/App.js
+++ src/App.js
@@ -2,12 +2,7 @@ import React from 'react';
 import Calc from './components/calc.js';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { result: 0, num1: '', num2: '' };
-  }
-
-  render() {
+    render() {
     return (
     <Calc/>
 
