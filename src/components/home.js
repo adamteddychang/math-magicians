5511e9524d214a72266393238e45b4e3120ad54e
adamteddychang <adamchang0725@gmail.com>
2021-12-01 12:02:52 +0100

    Completed full page webapp

---
+++ src/components/home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './home.css';
+
+export default function Home() {
+  return (
+  <div className="home">
+    <h1>Welcome to this webApp!</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+      Odio eum labore sequi. Qui dolorum
+      enim deserunt minima ratione, consequuntur quas esse culpa aliquam rem,
+      accusantium consectetur
+      labore odio repellat consequatur?</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+      Odio eum labore sequi. Qui dolorum
+      enim deserunt minima ratione, consequuntur quas esse culpa aliquam rem,
+      accusantium consectetur
+      labore odio repellat consequatur?</p>
+  </div>
+  );
+}
---
+++ src/components/nav.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './nav.css';
+
+export default function Nav() {
+  return (
+    <nav>
+      <h1>Math Magicians</h1>
+      <ul className="navContain">
+        <Link to='/'>
+        <li className="nav-item">Home</li>
+        </Link>
+        <Link to='/calc'>
+        <li className="nav-item">Calculator</li>
+        </Link>
+        <Link to='/quote'>
+        <li className="nav-item">Quote</li>
+        </Link>
+      </ul>
+    </nav>
+  );
+}
