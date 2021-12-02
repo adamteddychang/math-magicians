a7e792f76d82e61fbe0cec1a4d9e107dc31451ae
adamteddychang <adamchang0725@gmail.com>
2021-12-02 13:35:59 +0100

    remove unnecessary imports

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,5 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-
 import App from './App.js';
 import '@testing-library/jest-dom';
 
--- src/components/nav.test.js
+++ src/components/nav.test.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import renderer from 'react-test-renderer';
-import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import Nav from './nav.js';
 
 it('renders nav', () => {
